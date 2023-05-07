a7674e2096b8473e7bdda0728d09940ea0eeb9f3
Matheus Fontana <mbfontana@outlook.com>
2023-05-07 09:56:23 -0300

    Header first sketch

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,7 +1,19 @@
+import { Container } from "reactstrap";
 import styles from "./styles.module.scss";
 
 const Header = () => {
-  return <></>;
+  return (
+    <div className={styles.navBackground}>
+      <Container className={styles.container}>
+        <p>LOGO</p>
+        <div>
+          <p>ICON1</p>
+          <p>ICON2</p>
+          <p>ICON3</p>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Header;
