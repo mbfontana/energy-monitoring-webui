4e2d7ef20eb7b922511f2b3fcbdffd396aa1fed2
Matheus Fontana <mbfontana@outlook.com>
2023-05-07 09:55:53 -0300

    Loading Spinner component for Monitoring Card

---
+++ src/components/common/LoadingSpinner/index.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "reactstrap";
+import styles from "./styles.module.scss";
+
+const LoadingSpinner = () => {
+  return <span className={styles.loader}></span>;
+};
+
+export default LoadingSpinner;
