2238b185e417b1c3bffee21bd2612061584ac7cb
Matheus Fontana <mbfontana@outlook.com>
2023-05-07 09:55:42 -0300

    First Section layout and API Call

--- src/components/Home/FirstSection/index.tsx
+++ src/components/Home/FirstSection/index.tsx
@@ -1,8 +1,24 @@
+import consumptionService from "@/pages/api/consumptionService";
 import styles from "./styles.module.scss";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+import MonitoringCard from "../MonitoringCard";
+import { Container } from "reactstrap";
 
 const FirstSection = () => {
-  return <></>;
+  const { data } = useSWR("/appliances", consumptionService.getAppliances);
+
+  const appliances = data?.data;
+
+  return (
+    <Container className={styles.container}>
+      {appliances?.map((e) => (
+        <div key={e.id}>
+          <MonitoringCard id={e.id} name={e.name} />
+        </div>
+      ))}
+    </Container>
+  );
 };
 
 export default FirstSection;
