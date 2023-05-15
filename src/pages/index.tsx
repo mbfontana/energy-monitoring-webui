099b7189e508c96f0efec0d42a065d8b5382622d
Matheus Fontana <mbfontana@outlook.com>
2023-05-14 22:32:10 -0300

    Logo, new font and icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
-import Header from "@/components/common/Header";
+import SideLinks from "@/components/common/SideLinks";
+import styles from "../styles/Home.module.scss";
 import FirstSection from "@/components/Home/FirstSection";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Header />
+      <Link href="/">
+        <img alt="Logo" src="logo.svg" className={styles.logo} />
+      </Link>
       <FirstSection />
+      <SideLinks />
     </main>
   );
 }
